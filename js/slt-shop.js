3d707f72358bde6ce451b9ce78f81f3db3f845aa
fengyd <1070722821@qq.com>
2018-09-27 22:40:24 +0800

    第一次

---
+++ js/slt-shop.js
@@ -0,0 +1,16 @@
+// window.onload=function(){
+//     let click1=document.querySelectorAll(".click-one");
+//     let foot=document.querySelectorAll("footer");
+//     let p1=document.querySelectorAll(".add");
+//     // let boxactive=document.querySelector(".boxactive");
+//     // let active=document.querySelector(".active");
+//     console.log(click1,foot);
+//     click1[0].classList.add("boxactive");
+//     // p1[0].classList.add("active");
+//     for(let i=0;i<click1.length;i++){
+//         click1[i].onclick=function(){
+//
+//         }
+//     }
+
+}
