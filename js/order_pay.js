a416b1efedfb95c46a5f6e0a50d8172ed8f5f935
ZhWeiLan <893334661@qq.com>
2018-09-28 22:13:51 +0800

    order第二次

--- js/order_pay.js
+++ js/order_pay.js
@@ -1,4 +1,5 @@
 $(function(){
+    // 选项卡
     let lis=$("nav>ul li.lis");
     let bots=$("nav>ul>li>.bots");
     let list=$("main>.list");
@@ -10,6 +11,19 @@ $(function(){
         list.css("display","none").eq(i).css("display","block");
     })
     lis.eq(1).triggerHandler("click");
+    // 点击跳转
+    let serve=$(".list_serve>dl");
+    serve.click(function () {
+        location.href="order_wait.html";
+    })
+    let pay=$(".list_serve .sel_pay");
+    pay.click(function () {
+        location.href="order_write.html";
+    })
+    let assess=$(".list_assess .sel_pay");
+    assess.click(function () {
+        location.href="order_assess.html";
+    })
 })
 
 
