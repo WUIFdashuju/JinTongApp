127dd013bab76b6a1a311302ebebde5ea7a5b9c4
chenjunru123 <1135040160@qq.com>
2018-09-29 18:18:02 +0800

    个人中心后3

---
+++ js/personzdyje.js
@@ -0,0 +1,19 @@
+// $(function () {
+//     let dianji3=$(".body .box");
+//     // console.log(dianji3);
+//     dianji3.click(function () {
+//         let i= $(this).index();
+//         dianji3.removeClass("active").eq(i).addClass("active");
+//     });
+//     /////////////////////////////////////////////////////////////////
+//     let val=$(".body .box p span.price");
+//
+//     let kuang=$(".foot input");
+//     let shuzi=kuang.text();
+//     console.log(val, kuang, shuzi);
+//     dianji3.click(function () {
+//         let index = dianji3.index();
+//        kuang.val(val.eq(index).html);
+//
+//     });
+// })
